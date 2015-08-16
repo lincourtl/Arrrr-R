8551035ee2dc2dfa575df74762d7fd17f0bf11ac
Leo Lincourt <leolincourt@gmail.com>
2015-08-16 17:14:40 -0400

    Yup. That does it.

--- crumbrycode.r
+++
@@ -1,9 +0,0 @@
-crumbydata <- c(1:360)
-morecrumbs <- c("foo","bar","baz","bum")
-bigcrumbs <- crumbydata * pi^2
-bigcrumbs
-tinycrumbs <- crumbydata / pi^10
-tinycrumbs
-badcrumbs <- crumbydata / 0
-badcrumbs
-save.image("mydata.RData")
